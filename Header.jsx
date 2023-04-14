d9693b24dc50af9957e13bdb6757183083418085
Kritika Kaushal <96040841+kritika-kaushal@users.noreply.github.com>
2023-04-14 11:38:54 +0530

    Add files via upload
    Made some changes

---
+++ Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { nav } from '../../assets/data/data'
+import logo from "../../assets/images/logo.png"
+import "./header.css"
+import { Link } from 'react-router-dom'
+import User from './User'
+
+const Header = () => {
+    window.addEventListener("scroll", function(){
+        const header = this.document.querySelector(".header")
+        header.classList.toggle("active", this.window, this.scrollY > 100)
+    })
+  return (
+    <>
+    <header className='header'>
+        <div className="scontainer flex">
+            <div className="logo">
+                <img src={logo} alt="logo" width="100px" />
+            </div>
+            <nav>
+                <ul>
+                    {nav.map((link) =>(
+                        <li key= {link.id}>
+                            <Link to={link.url}>{link.text}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+            <div className="account flexCenter">
+                <User/>
+            </div>
+        </div>
+    </header>
+
+    <section></section>
+    </>
+  )
+}
+
+export default Header
