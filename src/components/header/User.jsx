f5adc752bab10cf8b61765fb02e370e7e16527ab
Kritika Kaushal <96040841+kritika-kaushal@users.noreply.github.com>
2023-04-17 11:54:27 +0530

    Made some changes to UI

--- src/components/header/User.jsx
+++ src/components/header/User.jsx
@@ -2,17 +2,13 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { RiImageAddLine } from 'react-icons/ri';
 import { IoSettingsOutline } from 'react-icons/io5';
-import { BsBagCheck } from 'react-icons/bs';
-import { AiOutlineHeart } from 'react-icons/ai';
-import { GrHelp } from 'react-icons/gr';
 import { BiLogOut } from 'react-icons/bi';
-
+import profile from "../../assets/images/profile.jpg"
 
 
 const User = () => {
     const user = true
-
-    const [profileOpen, setProfileOpen] = useState(true)
+    const [profileOpen, setProfileOpen] = useState(false)
         
         const close = () => {
             setProfileOpen(false)
@@ -23,15 +19,15 @@ const User = () => {
         {user ? (
          <>
         <button className='img' onClick={() =>setProfileOpen(!profileOpen)}>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ4bP1gBKfj-YgEstclXtEPBnt71aqx9c2kw&usqp=CAU' alt='images' />
+        <img src={profile} alt='images' />
         </button>
 
         {profileOpen && (
             <div className="openProfile boxItems" onClick={close}>
-            <Link to="account">
+            <Link to="/account">
                 <div className="image">
                     <div className="img">
-                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ4bP1gBKfj-YgEstclXtEPBnt71aqx9c2kw&usqp=CAU' alt='images' />
+                    <img src={profile} alt='images' />
                     </div>
                     <div className="text">
                         <h4>Zayn Malik</h4>
@@ -45,22 +41,12 @@ const User = () => {
                 <h4>Create Post</h4>
             </button>
             </Link>
+            <Link to="/account">
             <button className='box'>
                 <IoSettingsOutline className='icon'/>
                 <h4>My Account</h4>
             </button>
-            <button className='box'>
-                <BsBagCheck className='icon'/>
-                <h4>My Order</h4>
-            </button>
-            <button className='box'>
-                <AiOutlineHeart className='icon'/>
-                <h4>Wishlist</h4>
-            </button>
-            <button className='box'>
-                <GrHelp className='icon'/>
-                <h4>Help</h4>
-            </button>
+            </Link>
             <button className='box'>
                 <BiLogOut className='icon'/>
                 <h4>Log Out</h4>
@@ -69,7 +55,7 @@ const User = () => {
         )}
         </>
         ) : (
-            <Link to = "/login">
+            <Link to = "/account">
             <button>My Account</button>
             </Link>
         )}
